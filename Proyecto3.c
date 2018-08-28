fdab381c248a94ee9ed07e2dffa1bfb2d0923a1c
Hector <hectoreduardo354@hotmail.com>
2018-08-28 17:09:26 -0500

    medio fuciona

--- Proyecto3.c
+++ Proyecto3.c
@@ -4,13 +4,29 @@
     el usuario.
 */
 #include <stdio.h>
+//--------------------------------------------------
+//void ingresar_contacto();
+
+typedef struct{
+  char nombre[20][1];
+  int edad;
+  char direccion;
+  char telefono;
+}Contacto;
+
+
+//--------------------------------------------------
 int main(int argc, char const *argv[]) {
+
   int opcion = 0;
   printf("Eliga una opcion:\n" );
   scanf("%d",&opcion);
   switch (opcion) {
-    case 1:
+    case 1:;
       //ingresar contactos
+      Contacto Hector;
+      scanf("%s",Hector.nombre);
+      printf("%s\n",Hector.nombre );
       break;
     case 2:
       // consultar contacto
@@ -26,6 +42,16 @@ int main(int argc, char const *argv[]) {
       return 0;
     default:
       printf("Opcion invalida\n");
+
+  }
+}
+
+void ingresar_contacto() {
+  int numeroDeContactos = 0;
+  printf("Cuantos contactos va a ingresar: ");
+  scanf("%i",&numeroDeContactos);
+
+  for (size_t i = 0; i < numeroDeContactos; i++) {
 
   }
 }
