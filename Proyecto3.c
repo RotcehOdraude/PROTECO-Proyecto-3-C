983df32fd312ed37aef45d6684e500e5d36e88f0
Hector <hectoreduardo354@hotmail.com>
2018-08-27 22:48:18 -0500

    Hice un menu básico con switch para darle estructura al menú de opciones.

--- Proyecto3.c
+++ Proyecto3.c
@@ -1,5 +1,31 @@
+/* Esta es una agenda simple en lenguaje C.
+  Permite:
+    Almacenar datos personales de un número de personas dado por
+    el usuario.
+*/
 #include <stdio.h>
 int main(int argc, char const *argv[]) {
-  /* code */
-  return 0;
+  int opcion = 0;
+  printf("Eliga una opcion:\n" );
+  scanf("%d",&opcion);
+  switch (opcion) {
+    case 1:
+      //ingresar contactos
+      break;
+    case 2:
+      // consultar contacto
+      break;
+    case 3:
+      // modificar contacto
+      break;
+    case 4:
+      // mostrar todos los contactos
+      break;
+    case 5:
+      // salir
+      return 0;
+    default:
+      printf("Opcion invalida\n");
+
+  }
 }
